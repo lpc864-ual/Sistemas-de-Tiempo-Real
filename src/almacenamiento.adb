a60893ddbfba13bcef98a7eb267037f069ce7258
Luis Alexander Peraza Corobo <lpc864@inlumine.ual.es>
2024-03-22 11:36:18 +0100

    almacenamiento documentacion

--- src/almacenamiento.adb
+++ src/almacenamiento.adb
@@ -1,13 +1,32 @@
+-- Realizamos el cuerpo del paquete que estaremos utilizando
+-- para el almacenamiento de datos dentro de archivos de texto
+
+-- Incluimos los paquetes Ada.Text_IO, Ada.Float_Text_IO y ADA.Integer_Text_IO
+-- porque estaremos trabajando con archivos de texto, valores flotantes y enteros
+-- respectivamente
+
 with Ada.Text_IO, Ada.Float_Text_IO, ADA.Integer_Text_IO;
 use Ada.Text_IO, Ada.Float_Text_IO, ADA.Integer_Text_IO;
 
 package body almacenamiento is
 
+   -- Procedimiento que guardará la información generada de los diferentes
+   -- sensores en archivos de texto. La informacion relacionada con los datos se
+   -- guardara en un archivo dedicado a los datos, mientras que la informacion
+   -- relacionada con los errores se guardara en un archivo dedicado a los errores
+   -- El parametro k servirá como identificador de la iteracion donde estamos.
+   -- El parametro flag servirá para identificar la presencia de una advertencia
    procedure Guardar_Datos(output_data, output_error : File_Type; k : Integer; ST1, ST2, ST3, ST4, SC1, SC2, SR1, SD1 : Float; flag : Boolean) is
    begin
+
+      -- Si k es uno, entonces mostramos la cabecera
       if (k = 1) then
          Put_Line(output_data, ("          K       ST1       ST2       ST3       ST4       SC1       SC2       SR1       SD1"));
       end if;
+
+      -- Escribimos los diferentes valores con un espaciado entre ellos
+      -- Los valores flotantes seran mostrados con tres valores en la parte entera, tres valores en la parte decimal
+      -- y sin notacion cientifica
       Put(output_data, k);
       Put(output_data, "     ");
       Put(output_data, ST1, 3, 2, 0);
@@ -26,12 +45,15 @@ package body almacenamiento is
       Put(output_data, "    ");
       Put(output_data, SD1, 3, 2, 0);
 
+      -- Si flag es true, entonces tenemos una advertencia
       if (flag) then
           Put(output_error, "WARNING: ST2 > 98.0 (k = " & k'Image & ")");
          Put_Line(output_error, "");
       end if;
 
+      -- Escribimos un salto de linea para que cuando vuelva a ser llamado el procedimiento
+      -- los datos que sean escritos lo sean en una linea diferente
       Put_Line(output_data, "");
-   end Guardar_Datos;
 
+   end Guardar_Datos;
 end almacenamiento;
--- src/almacenamiento.ads
+++ src/almacenamiento.ads
@@ -1,16 +1,20 @@
--- Realizamos la especificacion del paquete que incluye los tipos de datos,
--- procedimientos y funciones que estaremos utilizando en el main.adb para
--- el procesamiento de los datos procedente de la instalación destiladora
+-- Realizamos la especificacion del paquete que estaremos utilizando
+-- para el almacenamiento de datos dentro de archivos de texto
 
--- Para evitar problemas al trabajar con tipos, entonces siempre estaremos
--- trabajando con datos de tipo float
+-- Incluimos los paquetes ADA.Text_IO porque estaremos trabajando con
+-- archivos de texto
 
 with ADA.Text_IO;
 use ADA.Text_IO;
 
 package almacenamiento is
 
-   -- Definimos las funciones que estaremos utilizando en el main.adb
+   -- Procedimiento que guardará la información generada de los diferentes
+   -- sensores en archivos de texto. La informacion relacionada con los datos se
+   -- guardara en un archivo dedicado a los datos, mientras que la informacion
+   -- relacionada con los errores se guardara en un archivo dedicado a los errores
+   -- El parametro k servirá como identificador de la iteracion donde estamos.
+   -- El parametro flag servirá para identificar la presencia de una advertencia
   procedure Guardar_Datos(output_data, output_error: File_Type; k : Integer; ST1, ST2, ST3, ST4, SC1, SC2, SR1, SD1 : Float; flag : Boolean);
 
 end almacenamiento;
