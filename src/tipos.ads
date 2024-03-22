e7e68d0a366b565b2553dae4a08cd5074cc84398
Luis Alexander Peraza Corobo <lpc864@inlumine.ual.es>
2024-03-22 12:31:45 +0100

    tipos documentacion

--- src/tipos.ads
+++ src/tipos.ads
@@ -1,14 +1,9 @@
--- Realizamos la especificacion del paquete que incluye los tipos de datos,
--- procedimientos y funciones que estaremos utilizando en el main.adb para
--- el procesamiento de los datos procedente de la instalación destiladora
-
--- Para evitar problemas al trabajar con tipos, entonces siempre estaremos
--- trabajando con datos de tipo float
+-- Realizamos la especificacion del paquete que estaremos utilizando
+-- para crear los diferentes tipos de datos que estaremos utilizando
 
 package tipos is
 
-   -- Definimos las funciones que estaremos utilizando en el main.adb
+   -- Se crea el tipo de dato Coleccion que es un array de tamaño indefinido
    type Coleccion is array (Integer range<>) of Float;
-   --type Matrix is array (Integer range<>, Integer range<>) of Float;
 
 end tipos;
