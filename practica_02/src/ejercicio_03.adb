c1bdb2fd2e2247bf3e859fe26f5c4c9060288af3
Luis Alexander Peraza Corobo <lpc864@inlumine.ual.es>
2024-05-02 18:06:03 +0200

    apartado c mejora

--- practica_02/src/ejercicio_03.adb
+++ practica_02/src/ejercicio_03.adb
@@ -1,14 +1,21 @@
-with sistema_control_campo_solar, sistema_control_modulo_md, sistema_seguridad, Ada.Text_IO;
-use sistema_control_campo_solar, sistema_control_modulo_md, sistema_seguridad, Ada.Text_IO;
+with sistema_control_campo_solar, sistema_control_modulo_md, sistema_seguridad, Ada.Text_IO, Ada.Integer_Text_IO;
+use sistema_control_campo_solar, sistema_control_modulo_md,sistema_seguridad, Ada.Text_IO, Ada.Integer_Text_IO;
 
 procedure ejercicio_03
 is
+   iteracion : Integer := 1;
 begin
-   --loop
-   ejecutar_proceso_control_campo_solar;
-   Put_Line("");
-   ejecutar_proceso_control_modulo_md;
-   Put_Line("");
-   ejecutar_proceso_seguridad;
-   --end loop;
+   loop
+      Put_Line("");
+      Put_Line("Iteracion " & Integer'Image(iteracion));
+      Put_Line("");
+      ejecutar_proceso_control_campo_solar;
+      Put_Line("");
+      ejecutar_proceso_control_modulo_md;
+      Put_Line("");
+      ejecutar_proceso_seguridad;
+      Put_Line("");
+      iteracion := iteracion + 1;
+      delay 5.0;
+   end loop;
 end ejercicio_03;
